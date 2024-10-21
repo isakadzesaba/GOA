49a5a10f87b20389e1901fbb3ce55d17641cb900
isakadzesaba <sabaisakadze99@gmail.com>
2024-10-21 17:28:19 +0400

    commit

--- Day 100/homework/homework.py
+++
@@ -1,9 +0,0 @@
-# guess = input("Enter your password: ")
-# password = "123"
-
-# while guess != password:
-#     guess = input("Enter your password: ")
-# if guess == password:
-#     print("Acsses granted!")
-
-
